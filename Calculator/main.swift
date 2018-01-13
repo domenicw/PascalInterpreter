a90b13ee72da015680d74bc21ff8a19ab95dc60b
Domenic Wüthrich <domenic.software@gmail.com>
2018-01-13 14:46:18 +0100

    Initial Commit

---
+++ Calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Domenic Wüthrich on 13.01.18.
+//  Copyright © 2018 Domenic Wüthrich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
